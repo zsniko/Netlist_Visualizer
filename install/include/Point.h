3cbfc4eb8993cf7a69f57e915556b89b7565c37e
zsniko <120737831+zsniko@users.noreply.github.com>
2023-11-22 00:14:51 +0100

    TME7 run

---
+++ install/include/Point.h
@@ -0,0 +1,42 @@
+// -*- explicit-buffer-name: "Point.h<M1-MOBJ/7>" -*-
+
+#ifndef NETLIST_POINT_H
+#define NETLIST_POINT_H
+
+#include <iostream>
+#include "Indentation.h"
+
+namespace Netlist {
+
+
+  class Point {
+    public:
+      inline         Point     ();
+      inline         Point     ( int x, int y);
+      inline  int    getX      () const;
+      inline  int    getY      () const;
+      inline  Point& setX      ( int );
+      inline  Point& setY      ( int );
+      inline  Point& translate ( int dx, int dy );
+      inline  Point& translate ( const Point& );
+    private:
+      int  x_;
+      int  y_;
+  };
+
+  inline         Point::Point     () : x_(0), y_(0) { }
+  inline         Point::Point     ( int x, int y) : x_(x), y_(y) { }
+  inline  int    Point::getX      () const { return x_; }
+  inline  int    Point::getY      () const { return y_; }
+  inline  Point& Point::setX      ( int x )  { x_ = x; return *this; } 
+  inline  Point& Point::setY      ( int y )  { y_ = y; return *this; }
+  inline  Point& Point::translate ( int dx, int dy ) { x_+=dx; y_+=dy; return *this; }
+  inline  Point& Point::translate ( const Point& p ) { return translate( p.getX(), p.getY() ); }
+
+  inline  std::ostream& operator<< ( std::ostream& stream, const Point& p )
+  { stream << "<Point " << p.getX() << " " << p.getY() << ">"; return stream; }
+
+
+}  // Netlist namespace.
+
+#endif  // NETLIST_POINT_H
